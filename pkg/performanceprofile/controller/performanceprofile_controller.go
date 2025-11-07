7458c303fc9b1f1b0e03ccb638f287fa9f1e22d2
Martin Sivak <msivak@redhat.com>
2025-11-07 12:30:59 +0100

    Repeat the attempt to set PerfProfile status after failure

--- pkg/performanceprofile/controller/performanceprofile_controller.go
+++ pkg/performanceprofile/controller/performanceprofile_controller.go
@@ -504,13 +504,15 @@ func (r *PerformanceProfileReconciler) Reconcile(ctx context.Context, req ctrl.R
 		conditions := status.GetDegradedConditions(status.ConditionReasonComponentsCreationFailed, err.Error())
 		if err := r.StatusWriter.Update(ctx, instance, conditions); err != nil {
 			klog.Errorf("failed to update performance profile %q status: %v", instance.GetName(), err)
-			return reconcile.Result{}, err
+			return reconcile.Result{RequeueAfter: 10 * time.Second}, err
 		}
 		return reconcile.Result{}, err
 	}
 	err = r.StatusWriter.UpdateOwnedConditions(ctx, instance)
 	if err != nil {
 		klog.Errorf("failed to update performance profile %q status: %v", instance.GetName(), err)
+		return reconcile.Result{RequeueAfter: 10 * time.Second}, err
+
 	}
 	return ctrl.Result{}, nil
 }
@@ -540,7 +542,7 @@ func (r *PerformanceProfileReconciler) getAndValidateMCP(ctx context.Context, in
 		conditions := status.GetDegradedConditions(status.ConditionFailedToFindMachineConfigPool, err.Error())
 		if err := r.StatusWriter.Update(ctx, profile, conditions); err != nil {
 			klog.Errorf("failed to update performance profile %q status: %v", profile.GetName(), err)
-			return nil, &reconcile.Result{}, err
+			return nil, &reconcile.Result{RequeueAfter: 10 * time.Second}, err
 		}
 		return nil, &reconcile.Result{}, nil
 	}
@@ -549,7 +551,7 @@ func (r *PerformanceProfileReconciler) getAndValidateMCP(ctx context.Context, in
 		conditions := status.GetDegradedConditions(status.ConditionBadMachineConfigLabels, err.Error())
 		if err := r.StatusWriter.Update(ctx, profile, conditions); err != nil {
 			klog.Errorf("failed to update performance profile %q status: %v", profile.GetName(), err)
-			return nil, &reconcile.Result{}, err
+			return nil, &reconcile.Result{RequeueAfter: 10 * time.Second}, err
 		}
 		return nil, &reconcile.Result{}, nil
 	}
